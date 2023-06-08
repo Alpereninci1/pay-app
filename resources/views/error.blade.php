ffe08fbab4f328470d237eadbdb93229ffde27e9
Alpereninci1 <alperen25inci@hotmail.com>
2023-06-08 17:10:57 +0300

    Success and error page has edited.

--- resources/views/error.blade.php
+++ resources/views/error.blade.php
@@ -1,15 +1,21 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="UTF-8">
-    <title>Response</title>
-</head>
-<body>
-Error !!!!
-<div>
-    <p>
-        {!! $data !!}
-    </p>
+<div class="image-container set-full-height">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-8 col-sm-offset-2">
+                <div class="wizard-container">
+                    <div class="card wizard-card">
+
+                        <div class="icon-circle" style="border: 3px solid red; margin-top:10px">
+                            <i class="ti-close" style="color: red"></i>
+                        </div>
+                        <h4 class="info-text"> Hay aksi!</h4>
+                        <h5 class="text-center"> Ödeme işleminde bir hata oluştu.</h5>
+                        <h5 class="text-center"> {{$data}}</h5>
+
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
-</body>
-</html>
--- resources/views/success.blade.php
+++ resources/views/success.blade.php
@@ -1,10 +1,30 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="UTF-8">
-    <title>Response</title>
-</head>
-<body>
- Success !!!!
-</body>
-</html>
+{{--<!DOCTYPE html>--}}
+{{--<html>--}}
+{{--<head>--}}
+{{--    <meta charset="UTF-8">--}}
+{{--    <title>Response</title>--}}
+{{--</head>--}}
+{{--<body>--}}
+{{-- Success !!!!--}}
+{{--</body>--}}
+{{--</html>--}}
+
+<div class="image-container set-full-height">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-8 col-sm-offset-2">
+                <div class="wizard-container">
+                    <div class="card wizard-card">
+
+                        <div class="icon-circle" style="border: 3px solid green; margin-top:10px">
+                            <i class="ti-check" style="color: green"></i>
+                        </div>
+                        <h4 class="info-text"> Tebrikler!</h4>
+                        <h5 class="text-center"> Ödeme işleminiz başarıyla tamamlandı.</h5>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
